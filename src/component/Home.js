2c50d7880ab8369d14b9df87a29d2dc1a980ca8d
pratyushmishraaa <pratyushmishra287@gmail.com>
2023-01-11 22:05:04 +0530

    origin changes to Boarding

--- src/component/Home.js
+++ src/component/Home.js
@@ -11,13 +11,13 @@ const Home = () => {
       <div>
       
          <h1>
-         Welcome to FlyAir
+         Welcome to FlyAirways
          </h1>
          <form>
          
          <label>Traveler Name: </label><input type="text" name="uname"  /><br />
          <label>Traveler's Age: </label><input type="text" name="age"  /><br />
-         <label className='ori'>Origin: </label><input type="text" name="origin"  /><br />
+         <label className='ori'>Boarding: </label><input type="text" name="origin"  /><br />
          <label className='des'>Destination: </label><input type="text" name="origin"  /><br />
          <label>Date of Journey: </label><input type="date" name="origin"  /><br />
          <button className='btn'>Book Now</button>
