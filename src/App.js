307fb1b9e01fb92405518821eda11c5499e4a441
pratyushmishraaa <pratyushmishra287@gmail.com>
2023-01-11 21:40:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './component/Home';
+
+function App() {
+  return (
+    <div>
+    <Home />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+   return(
+      <div>
+      
+      
+      <div className="form">
+      <img id='pic' src="https://images.moneycontrol.com/static-mcnews/2021/07/Airline.jpg?impolicy=website&width=770&height=431" alt="" />
+      <div>
+      
+         <h1>
+         Welcome to FlyAir
+         </h1>
+         <form>
+         
+         <label>Traveler Name: </label><input type="text" name="uname"  /><br />
+         <label>Traveler's Age: </label><input type="text" name="age"  /><br />
+         <label className='ori'>Origin: </label><input type="text" name="origin"  /><br />
+         <label className='des'>Destination: </label><input type="text" name="origin"  /><br />
+         <label>Date of Journey: </label><input type="date" name="origin"  /><br />
+         <button className='btn'>Book Now</button>
+
+         
+         
+         </form>
+           
+      </div>
+     <form>
+       
+
+
+         
+       
+       
+       
+     </form>
+   </div>
+
+            
+      </div>
+   );
+};
+export default Home;
